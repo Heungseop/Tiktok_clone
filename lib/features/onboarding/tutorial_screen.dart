fcaa266d30a35a2e848f1a52ddcd299dafa0a965
heungseop <imt300@naver.com>
2023-05-24 12:56:20 +0900

    5.4 AnimatedCrossFade

--- lib/features/onboarding/tutorial_screen.dart
+++ lib/features/onboarding/tutorial_screen.dart
@@ -1,6 +1,11 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:tiktok_clone/constants/gaps.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
+
+enum Direction { right, left }
+
+enum Page { first, sencond }
 
 class TutorialScreen extends StatefulWidget {
   const TutorialScreen({super.key});
@@ -10,23 +15,51 @@ class TutorialScreen extends StatefulWidget {
 }
 
 class _TutorialScreenState extends State<TutorialScreen> {
+  Direction _direction = Direction.right;
+  Page _showingPage = Page.first;
+
+  void _onPanUpdate(DragUpdateDetails details) {
+    print(details);
+    const minDragOffset = 20;
+    if (details.delta.dx > minDragOffset) {
+      //to the right
+      setState(() {
+        _direction = Direction.right;
+      });
+    } else if (details.delta.dx < -minDragOffset) {
+      // to the left
+      setState(() {
+        _direction = Direction.left;
+      });
+    }
+  }
+
+  void _onPanEnd(DragEndDetails detail) {
+    if (_direction == Direction.left) {
+      setState(() {
+        _showingPage = Page.sencond;
+      });
+    } else {
+      setState(() {
+        _showingPage = Page.first;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      // TabBarView - TabPageSelector 같이 사용하려면 DefaultTabController 하위에 사용해야 한다
-      length: 3,
+    return GestureDetector(
+      onPanUpdate: _onPanUpdate,
+      onPanEnd: _onPanEnd,
       child: Scaffold(
-        body: SafeArea(
-          child: TabBarView(
-            children: [
-              Padding(
-                padding: const EdgeInsets.symmetric(
-                  horizontal: Sizes.size24,
-                ),
-                child: Column(
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: Sizes.size24),
+          child: SafeArea(
+            child: AnimatedCrossFade(
+                firstChild: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: const [
-                    Gaps.v52,
+                    Gaps.v80,
                     Text(
                       "Watch cool videos!",
                       style: TextStyle(
@@ -43,73 +76,55 @@ class _TutorialScreenState extends State<TutorialScreen> {
                     ),
                   ],
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(
-                  horizontal: Sizes.size24,
-                ),
-                child: Column(
+                secondChild: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: const [
-                    Gaps.v52,
-                    Text(
-                      "Follow the rules",
-                      style: TextStyle(
-                        fontSize: Sizes.size40,
-                        fontWeight: FontWeight.bold,
+                    Gaps.v80,
+                    FractionallySizedBox(
+                      widthFactor: 1,
+                      child: Text(
+                        "Follow the Rules",
+                        style: TextStyle(
+                          fontSize: Sizes.size40,
+                          fontWeight: FontWeight.bold,
+                        ),
                       ),
                     ),
                     Gaps.v16,
                     Text(
-                      "Videos are personalized for you based on what you watch, like, and share.",
+                      "Take care of one another! please!",
                       style: TextStyle(
                         fontSize: Sizes.size20,
                       ),
                     ),
                   ],
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(
-                  horizontal: Sizes.size24,
-                ),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: const [
-                    Gaps.v52,
-                    Text(
-                      "Enjoy the Ride",
-                      style: TextStyle(
-                        fontSize: Sizes.size40,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    Gaps.v16,
-                    Text(
-                      "Videos are personalized for you based on what you watch, like, and share.",
-                      style: TextStyle(
-                        fontSize: Sizes.size20,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ],
+                crossFadeState: _showingPage == Page.first
+                    ? CrossFadeState.showFirst
+                    : CrossFadeState.showSecond,
+                duration: const Duration(milliseconds: 300)),
           ),
         ),
         bottomNavigationBar: BottomAppBar(
-          child: Container(
+          child: Padding(
             padding: const EdgeInsets.symmetric(
-              vertical: Sizes.size48,
+              vertical: Sizes.size24,
+              horizontal: Sizes.size24,
             ),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: const [
-                TabPageSelector(
-                  color: Colors.white,
-                  selectedColor: Colors.black38,
-                ),
-              ],
+            child: AnimatedOpacity(
+              duration: const Duration(milliseconds: 300),
+              opacity: _showingPage == Page.first ? 0 : 1,
+              child: _showingPage == Page.first
+                  ? const SizedBox(
+                      height: 1,
+                    )
+                  : CupertinoButton(
+                      onPressed: () {
+                        print("onPressed!");
+                      },
+                      color: Theme.of(context).primaryColor,
+                      child: const Text("Enter the App!"),
+                    ),
             ),
           ),
         ),
