7b4e7af13a31c64d1572c237fa77984350537a0d
Heungseop <imt300@naver.com>
2023-06-07 14:00:18 +0900

    9.2 TabBar

--- lib/features/discover/discover_screen.dart
+++ lib/features/discover/discover_screen.dart
@@ -1,16 +1,59 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:tiktok_clone/constants/sizes.dart';
 
 class DiscoverScreen extends StatelessWidget {
-  const DiscoverScreen({super.key});
+  DiscoverScreen({super.key});
+  final tabs = [
+    "Top",
+    "Users",
+    "Videos",
+    "Sounds",
+    "LIVE",
+    "Shopping",
+    "Brands",
+  ];
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Discover"),
-      ),
+    return DefaultTabController(
+      length: tabs.length,
+      child: Scaffold(
+          appBar: AppBar(
+            elevation: 1,
+            centerTitle: true,
+            title: const Text("Discover"),
+            bottom: TabBar(
+              splashFactory: NoSplash.splashFactory,
+              padding: const EdgeInsets.symmetric(
+                horizontal: Sizes.size16,
+              ),
+              isScrollable: true,
+              labelColor: Colors.black,
+              unselectedLabelColor: Colors.grey.shade500,
+              indicatorColor: Colors.black,
+              labelStyle: const TextStyle(
+                fontWeight: FontWeight.w600,
+                fontSize: Sizes.size16,
+              ),
+              tabs: [
+                for (var tab in tabs)
+                  Tab(
+                    text: tab,
+                  )
+              ],
+            ), // PreferredSizeWidget type을 가져야한다. (= 특정한 크기를 가지려고 하지만 자식요소의 크기를 제한하지 않는 위젯)
+          ),
+          body: TabBarView(
+            children: [
+              for (var tab in tabs)
+                Center(
+                  child: Text(
+                    tab,
+                    style: const TextStyle(fontSize: 28),
+                  ),
+                )
+            ],
+          )),
     );
   }
 }
