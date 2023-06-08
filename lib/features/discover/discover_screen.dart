a832a10cbc64bfb9ab3b25018b9cdeff429be887
Heungseop <imt300@naver.com>
2023-06-08 11:19:44 +0900

    Squashed commit of the following:
    commit 8c0ad813a9c423aee9f326637cf732a4616edd06
    Author: Heungseop <imt300@naver.com>
    Date:   Thu Jun 8 11:13:53 2023 +0900
    
        9.5 code challenge2 manual searchField
    
    commit 2603bf6f4bf3f5ef70872f3054700c22428cc4ad
    Author: Heungseop <imt300@naver.com>
    Date:   Wed Jun 7 17:57:31 2023 +0900
    
        add dispose
    
    commit 67b9fd5ea9f976725437acccc80c5e12e2b4e8ef
    Author: Heungseop <imt300@naver.com>
    Date:   Wed Jun 7 17:54:51 2023 +0900
    
        9.5 CupertinoSearchTextField code challenge

--- lib/features/discover/discover_screen.dart
+++ lib/features/discover/discover_screen.dart
@@ -1,8 +1,17 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:tiktok_clone/constants/gaps.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
+
+final tabs = [
+  "Top",
+  "Users",
+  "Videos",
+  "Sounds",
+  "LIVE",
+  "Shopping",
+  "Brands",
+];
 
 class DiscoverScreen extends StatefulWidget {
   const DiscoverScreen({super.key});
@@ -11,21 +20,15 @@ class DiscoverScreen extends StatefulWidget {
   State<DiscoverScreen> createState() => _DiscoverScreenState();
 }
 
-class _DiscoverScreenState extends State<DiscoverScreen> {
-  final TextEditingController _textEditingController =
-      TextEditingController(text: "Initial Text");
+class _DiscoverScreenState extends State<DiscoverScreen>
+    with SingleTickerProviderStateMixin {
+  final TextEditingController _textEditingController = TextEditingController();
 
-  final tabs = [
-    "Top",
-    "Users",
-    "Videos",
-    "Sounds",
-    "LIVE",
-    "Shopping",
-    "Brands",
-  ];
+  late final TabController _tabController =
+      TabController(length: tabs.length, vsync: this);
 
   void _onSearchChanged(String value) {
+    // FocusScope.of(context).unfocus();
     print("_onSearchChanged : $value");
   }
 
@@ -33,133 +36,208 @@ class _DiscoverScreenState extends State<DiscoverScreen> {
     print("_onSearchSubmitted : $value");
   }
 
+  void _onSearchFieldReset() {
+    _textEditingController.clear();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    _tabController.addListener(() {
+      if (_tabController.indexIsChanging) {
+        FocusScope.of(context).unfocus();
+      }
+    });
+  }
+
   @override
   void dispose() {
     _textEditingController.dispose();
+    _tabController.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: tabs.length,
-      child: Scaffold(
-          resizeToAvoidBottomInset: false,
-          appBar: AppBar(
-            elevation: 1,
-            centerTitle: true,
-            title: CupertinoSearchTextField(
-              controller: _textEditingController,
-              onChanged: _onSearchChanged,
-              onSubmitted: _onSearchSubmitted,
-            ),
-            bottom: TabBar(
-              splashFactory: NoSplash.splashFactory,
-              padding: const EdgeInsets.symmetric(
-                horizontal: Sizes.size16,
-              ),
-              isScrollable: true,
-              labelColor: Colors.black,
-              unselectedLabelColor: Colors.grey.shade500,
-              indicatorColor: Colors.black,
-              labelStyle: const TextStyle(
-                fontWeight: FontWeight.w600,
-                fontSize: Sizes.size16,
-              ),
-              tabs: [
-                for (var tab in tabs)
-                  Tab(
-                    text: tab,
-                  )
-              ],
-            ), // PreferredSizeWidget type을 가져야한다. (= 특정한 크기를 가지려고 하지만 자식요소의 크기를 제한하지 않는 위젯)
-          ),
-          body: TabBarView(
+    return Scaffold(
+        resizeToAvoidBottomInset: false,
+        appBar: AppBar(
+          elevation: 1,
+          centerTitle: true,
+          // title: CupertinoSearchTextField(
+          //   controller: _textEditingController,
+          //   onChanged: _onSearchChanged,
+          //   onSubmitted: _onSearchSubmitted,
+          // ),
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              GridView.builder(
-                keyboardDismissBehavior:
-                    ScrollViewKeyboardDismissBehavior.onDrag,
-                itemCount: 20,
-                padding: const EdgeInsets.all(Sizes.size6),
-                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 2,
-                  crossAxisSpacing: Sizes.size10,
-                  mainAxisSpacing: Sizes.size10,
-                  childAspectRatio: 9 / 20,
-                ),
-                itemBuilder: (context, index) => Column(
-                  // color: Colors.teal,
-                  children: [
-                    Container(
-                      clipBehavior: Clip.hardEdge,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(Sizes.size4),
-                      ),
-                      child: AspectRatio(
-                        aspectRatio: 9 / 15,
-                        child: FadeInImage.assetNetwork(
-                            fit: BoxFit.cover,
-                            placeholder: "assets/images/IMG_4793.jpg",
-                            image:
-                                "https://source.unsplash.com/random/?$index"),
-                      ),
+              SizedBox(
+                width: MediaQuery.of(context).size.width - Sizes.size24 - 60,
+                height: Sizes.size40, // 인풋 높이
+                child: TextField(
+                  onSubmitted: _onSearchSubmitted,
+                  onChanged: _onSearchChanged,
+                  controller: _textEditingController,
+                  cursorColor: Theme.of(context).primaryColor,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(Sizes.size12),
+                      borderSide: BorderSide.none, // 라인제거
                     ),
-                    Gaps.v10,
-                    const Text(
-                      "This is a very long cation for my tiktok thet im upload just now currently.",
-                      maxLines: 2,
-                      overflow: TextOverflow.ellipsis,
-                      style: TextStyle(
-                        fontSize: Sizes.size16 + Sizes.size2,
-                        fontWeight: FontWeight.bold,
-                      ),
+                    filled: true, // 인풋 색상 채울지 여부
+                    fillColor: Colors.grey.shade200,
+                    prefixIcon: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: const [
+                        FaIcon(
+                          FontAwesomeIcons.magnifyingGlass,
+                          // color: Colors.grey.shade500,
+                          color: Colors.black,
+                          size: Sizes.size20,
+                        ),
+                      ],
                     ),
-                    Gaps.v8,
-                    DefaultTextStyle(
-                      style: TextStyle(
-                        color: Colors.grey.shade600,
-                        fontWeight: FontWeight.bold,
-                      ),
-                      child: Row(
-                        children: [
-                          const CircleAvatar(
-                            radius: 12,
-                            backgroundImage: NetworkImage(
-                                "https://avatars.githubusercontent.com/u/13977411?v=4"),
-                          ),
-                          Gaps.h4,
-                          const Expanded(
-                            child: Text(
-                              "my avatar is going to be very long",
-                              maxLines: 1,
-                              overflow: TextOverflow.ellipsis,
-                            ),
-                          ),
-                          Gaps.h4,
-                          FaIcon(
-                            FontAwesomeIcons.heart,
-                            size: Sizes.size16,
+                    suffixIcon: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        GestureDetector(
+                          onTap: _onSearchFieldReset,
+                          child: FaIcon(
+                            FontAwesomeIcons.solidCircleXmark,
                             color: Colors.grey.shade600,
+                            size: Sizes.size20,
                           ),
-                          Gaps.h2,
-                          const Text(
-                            "2.5M",
-                          ),
-                        ],
-                      ),
+                        ),
+                      ],
                     ),
-                  ],
+                    contentPadding: const EdgeInsets.symmetric(
+                      horizontal: Sizes.size12,
+                      vertical: Sizes.size10,
+                    ),
+                  ),
                 ),
               ),
-              for (var tab in tabs.skip(1))
-                Center(
-                  child: Text(
-                    tab,
-                    style: const TextStyle(fontSize: 28),
-                  ),
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: Sizes.size12),
+                child: FaIcon(
+                  FontAwesomeIcons.sliders,
+                  // color: Colors.grey.shade500,
+                  size: Sizes.size24,
+                ),
+              ),
+            ],
+          ),
+          bottom: TabBar(
+            controller: _tabController,
+            splashFactory:
+                NoSplash.splashFactory, //material design의 버튼을 누르면 퍼지는 효과 제거
+            padding: const EdgeInsets.symmetric(
+              horizontal: Sizes.size16,
+            ),
+            isScrollable: true,
+            labelColor: Colors.black,
+            unselectedLabelColor: Colors.grey.shade500,
+            indicatorColor: Colors.black,
+            labelStyle: const TextStyle(
+              fontWeight: FontWeight.w600,
+              fontSize: Sizes.size16,
+            ),
+            tabs: [
+              for (var tab in tabs)
+                Tab(
+                  text: tab,
                 )
             ],
-          )),
-    );
+          ), // PreferredSizeWidget type을 가져야한다. (= 특정한 크기를 가지려고 하지만 자식요소의 크기를 제한하지 않는 위젯)
+        ),
+        body: TabBarView(
+          controller: _tabController,
+          children: [
+            GridView.builder(
+              keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.onDrag,
+              itemCount: 20,
+              padding: const EdgeInsets.all(Sizes.size6),
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                crossAxisSpacing: Sizes.size10,
+                mainAxisSpacing: Sizes.size10,
+                childAspectRatio: 9 / 20,
+              ),
+              itemBuilder: (context, index) => Column(
+                // color: Colors.teal,
+                children: [
+                  Container(
+                    clipBehavior: Clip.hardEdge,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(Sizes.size4),
+                    ),
+                    child: AspectRatio(
+                      aspectRatio: 9 / 15,
+                      child: FadeInImage.assetNetwork(
+                          fit: BoxFit.cover,
+                          placeholder: "assets/images/IMG_4793.jpg",
+                          image: "https://source.unsplash.com/random/?$index"),
+                    ),
+                  ),
+                  Gaps.v10,
+                  const Text(
+                    "This is a very long cation for my tiktok thet im upload just now currently.",
+                    maxLines: 2,
+                    overflow: TextOverflow.ellipsis,
+                    style: TextStyle(
+                      fontSize: Sizes.size16 + Sizes.size2,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Gaps.v8,
+                  DefaultTextStyle(
+                    style: TextStyle(
+                      color: Colors.grey.shade600,
+                      fontWeight: FontWeight.bold,
+                    ),
+                    child: Row(
+                      children: [
+                        const CircleAvatar(
+                          radius: 12,
+                          backgroundImage: NetworkImage(
+                              "https://avatars.githubusercontent.com/u/13977411?v=4"),
+                        ),
+                        Gaps.h4,
+                        const Expanded(
+                          child: Text(
+                            "my avatar is going to be very long",
+                            maxLines: 1,
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                        ),
+                        Gaps.h4,
+                        FaIcon(
+                          FontAwesomeIcons.heart,
+                          size: Sizes.size16,
+                          color: Colors.grey.shade600,
+                        ),
+                        Gaps.h2,
+                        const Text(
+                          "2.5M",
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            for (var tab in tabs.skip(1))
+              Center(
+                child: Text(
+                  tab,
+                  style: const TextStyle(fontSize: 28),
+                ),
+              )
+          ],
+        ));
   }
 }
