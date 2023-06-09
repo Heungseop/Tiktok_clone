53759721d95a6900729dcbb2f1f23c4b082e3b49
Heungseop <imt300@naver.com>
2023-06-09 14:36:06 +0900

    11.2 AnimatedList

--- lib/features/inbox/chats_screen.dart
+++ lib/features/inbox/chats_screen.dart
@@ -10,6 +10,18 @@ class ChatsScreen extends StatefulWidget {
 }
 
 class _ChatsScreenState extends State<ChatsScreen> {
+  final GlobalKey<AnimatedListState> _key = GlobalKey<AnimatedListState>();
+
+  final List<int> _items = [];
+
+  void _addItem() {
+    if (_key.currentState != null) {
+      _key.currentState!.insertItem(_items.length,
+          duration: const Duration(milliseconds: 300));
+      _items.add(_items.length);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,43 +31,54 @@ class _ChatsScreenState extends State<ChatsScreen> {
         centerTitle: true,
         actions: [
           IconButton(
-            onPressed: () {},
+            onPressed: _addItem,
             icon: const FaIcon(
               FontAwesomeIcons.plus,
             ),
           )
         ],
       ),
-      body: ListView(
+      body: AnimatedList(
+        key: _key,
         padding: const EdgeInsets.symmetric(vertical: Sizes.size10),
-        children: [
-          ListTile(
-            leading: const CircleAvatar(
-              radius: 30,
-              foregroundImage: NetworkImage(
-                  "https://avatars.githubusercontent.com/u/3612017?v=4"),
-              child: Text("jinjoo"),
-            ),
-            title: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              crossAxisAlignment: CrossAxisAlignment.end,
-              children: [
-                const Text(
-                  "jinjoo",
-                  style: TextStyle(
-                    fontWeight: FontWeight.w600,
-                  ),
+        itemBuilder:
+            (BuildContext context, int index, Animation<double> animation) {
+          return FadeTransition(
+            opacity: animation,
+            key: UniqueKey(), // 리스트 타일에 유니크 키 부여
+            child: SizeTransition(
+              sizeFactor: animation,
+              child: ListTile(
+                leading: const CircleAvatar(
+                  radius: 30,
+                  foregroundImage: NetworkImage(
+                      "https://avatars.githubusercontent.com/u/13977411?v=4"
+                      // "https://avatars.githubusercontent.com/u/3612017?v=4"
+                      ),
+                  child: Text("jinjoo"),
                 ),
-                Text(
-                  "2:16 PM",
-                  style: TextStyle(
-                      color: Colors.grey.shade500, fontSize: Sizes.size12),
+                title: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    Text(
+                      "Jin $index",
+                      style: const TextStyle(
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                    Text(
+                      "2:16 PM",
+                      style: TextStyle(
+                          color: Colors.grey.shade500, fontSize: Sizes.size12),
+                    ),
+                  ],
                 ),
-              ],
+                subtitle: const Text("Don't forget to make video"),
+              ),
             ),
-            subtitle: const Text("Don't forget to make video"),
-          )
-        ],
+          );
+        },
       ),
     );
   }
