40a99c5e837a56dc8c4669552b1afea85b5307be
Heungseop <imt300@naver.com>
2023-06-09 15:04:32 +0900

    11.3 AnimatedList part Two

--- lib/features/inbox/chats_screen.dart
+++ lib/features/inbox/chats_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
+import 'package:tiktok_clone/features/inbox/chat_detail_screen.dart';
 
 class ChatsScreen extends StatefulWidget {
   const ChatsScreen({super.key});
@@ -13,13 +14,35 @@ class _ChatsScreenState extends State<ChatsScreen> {
   final GlobalKey<AnimatedListState> _key = GlobalKey<AnimatedListState>();
 
   final List<int> _items = [];
+  final Duration _duration = const Duration(milliseconds: 200);
 
   void _addItem() {
     if (_key.currentState != null) {
-      _key.currentState!.insertItem(_items.length,
-          duration: const Duration(milliseconds: 300));
+      _key.currentState!.insertItem(_items.length, duration: _duration);
       _items.add(_items.length);
     }
+  }
+
+  void _deleteItem(int index) {
+    if (_key.currentState != null) {
+      _key.currentState!.removeItem(
+        index,
+        (context, animation) => SizeTransition(
+          sizeFactor: animation,
+          child: Container(color: Colors.red, child: _makeTile(index)),
+        ),
+        duration: _duration,
+      );
+      _items.removeAt(index);
+    }
+  }
+
+  void _onChatTap() {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (context) => const ChatDetailScreen(),
+      ),
+    );
   }
 
   @override
@@ -48,38 +71,44 @@ class _ChatsScreenState extends State<ChatsScreen> {
             key: UniqueKey(), // 리스트 타일에 유니크 키 부여
             child: SizeTransition(
               sizeFactor: animation,
-              child: ListTile(
-                leading: const CircleAvatar(
-                  radius: 30,
-                  foregroundImage: NetworkImage(
-                      "https://avatars.githubusercontent.com/u/13977411?v=4"
-                      // "https://avatars.githubusercontent.com/u/3612017?v=4"
-                      ),
-                  child: Text("jinjoo"),
-                ),
-                title: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  children: [
-                    Text(
-                      "Jin $index",
-                      style: const TextStyle(
-                        fontWeight: FontWeight.w600,
-                      ),
-                    ),
-                    Text(
-                      "2:16 PM",
-                      style: TextStyle(
-                          color: Colors.grey.shade500, fontSize: Sizes.size12),
-                    ),
-                  ],
-                ),
-                subtitle: const Text("Don't forget to make video"),
-              ),
+              child: _makeTile(index),
             ),
           );
         },
       ),
+    );
+  }
+
+  ListTile _makeTile(int index) {
+    return ListTile(
+      onLongPress: () => _deleteItem(index),
+      onTap: _onChatTap,
+      leading: const CircleAvatar(
+        radius: 30,
+        foregroundImage:
+            NetworkImage("https://avatars.githubusercontent.com/u/13977411?v=4"
+                // "https://avatars.githubusercontent.com/u/3612017?v=4"
+                ),
+        child: Text("jinjoo"),
+      ),
+      title: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          Text(
+            "Jin $index",
+            style: const TextStyle(
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+          Text(
+            "2:16 PM",
+            style:
+                TextStyle(color: Colors.grey.shade500, fontSize: Sizes.size12),
+          ),
+        ],
+      ),
+      subtitle: const Text("Don't forget to make video"),
     );
   }
 }
