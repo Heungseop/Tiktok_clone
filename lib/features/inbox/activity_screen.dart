579cfbbcd6fa1cff4d98685fd06e8af00e60c1c6
Heungseop <imt300@naver.com>
2023-06-09 09:44:21 +0900

    10.3 onDismissed

--- lib/features/inbox/activity_screen.dart
+++ lib/features/inbox/activity_screen.dart
@@ -3,8 +3,28 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:tiktok_clone/constants/gaps.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
 
-class ActivityScreen extends StatelessWidget {
+// dismissible 위젯만 사용하면 사용자가 dismiss했을 때 화면에서만 없어지고 실제로는 위젯트리에 남아있기때문에
+// 소스 변경 후 핫 리로드 되면 에러가 난다 이를 위해
+// 1. stateless -> stateful 변경
+// 2. _notifications 생성
+// 3. Dismissible 을 리스트 for루프로 그려줌
+// 4. onDismissed 에서 삭제된 아이템을 리스트에서도 remove
+
+class ActivityScreen extends StatefulWidget {
   const ActivityScreen({super.key});
+
+  @override
+  State<ActivityScreen> createState() => _ActivityScreenState();
+}
+
+class _ActivityScreenState extends State<ActivityScreen> {
+  final List<String> _notifications = List.generate(20, (index) => "${index}h");
+
+  void _onDismissed(String noti) {
+    setState(() {
+      _notifications.remove(noti);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -27,87 +47,90 @@ class ActivityScreen extends StatelessWidget {
             ),
           ),
           Gaps.v14,
-          Dismissible(
-            key: const Key("x"),
-            background: Container(
-              alignment: Alignment.centerLeft,
-              color: Colors.green,
-              child: const Padding(
-                padding: EdgeInsets.only(
-                  left: Sizes.size10,
-                ),
-                child: FaIcon(
-                  FontAwesomeIcons.checkDouble,
-                  color: Colors.white,
-                  size: Sizes.size32,
-                ),
-              ),
-            ),
-            secondaryBackground: Container(
-              alignment: Alignment.centerRight,
-              color: Colors.red,
-              child: const Padding(
-                padding: EdgeInsets.only(
-                  right: Sizes.size10,
-                ),
-                child: FaIcon(
-                  FontAwesomeIcons.trashCan,
-                  color: Colors.white,
-                  size: Sizes.size32,
-                ),
-              ),
-            ),
-            child: ListTile(
-              // contentPadding: EdgeInsets.zero,
-              leading: Container(
-                width: Sizes.size52,
-                decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  color: Colors.white,
-                  border: Border.all(
-                    color: Colors.grey.shade400,
-                    width: Sizes.size1,
+          for (String noti in _notifications)
+            Dismissible(
+              key: Key(noti),
+              onDismissed: (direction) => _onDismissed(noti),
+              background: Container(
+                alignment: Alignment.centerLeft,
+                color: Colors.green,
+                child: const Padding(
+                  padding: EdgeInsets.only(
+                    left: Sizes.size10,
                   ),
-                ),
-                child: const Center(
                   child: FaIcon(
-                    FontAwesomeIcons.bell,
-                    color: Colors.black,
+                    FontAwesomeIcons.checkDouble,
+                    color: Colors.white,
+                    size: Sizes.size32,
                   ),
                 ),
               ),
-              title: RichText(
-                text: TextSpan(
-                  text: "Account updates:",
-                  style: const TextStyle(
-                    fontWeight: FontWeight.bold,
-                    color: Colors.black,
-                    fontSize: Sizes.size16,
+              secondaryBackground: Container(
+                alignment: Alignment.centerRight,
+                color: Colors.red,
+                child: const Padding(
+                  padding: EdgeInsets.only(
+                    right: Sizes.size10,
                   ),
-                  children: [
-                    const TextSpan(
-                      text: " Upload longer videos",
-                      style: TextStyle(
-                        fontWeight: FontWeight.normal,
-                      ),
-                    ),
-                    TextSpan(
-                      text: " 1h",
-                      style: TextStyle(
-                        fontWeight: FontWeight.normal,
-                        color: Colors.grey.shade500,
-                      ),
-                    ),
-                  ],
+                  child: FaIcon(
+                    FontAwesomeIcons.trashCan,
+                    color: Colors.white,
+                    size: Sizes.size32,
+                  ),
                 ),
               ),
-              trailing: const FaIcon(
-                FontAwesomeIcons.chevronRight,
-                size: Sizes.size16,
-                color: Colors.black,
+              child: ListTile(
+                minVerticalPadding: Sizes.size16,
+                // contentPadding: EdgeInsets.zero,
+                leading: Container(
+                  width: Sizes.size52,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Colors.white,
+                    border: Border.all(
+                      color: Colors.grey.shade400,
+                      width: Sizes.size1,
+                    ),
+                  ),
+                  child: const Center(
+                    child: FaIcon(
+                      FontAwesomeIcons.bell,
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
+                title: RichText(
+                  text: TextSpan(
+                    text: "Account updates:",
+                    style: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                      fontSize: Sizes.size16,
+                    ),
+                    children: [
+                      const TextSpan(
+                        text: " Upload longer videos",
+                        style: TextStyle(
+                          fontWeight: FontWeight.normal,
+                        ),
+                      ),
+                      TextSpan(
+                        text: noti,
+                        style: TextStyle(
+                          fontWeight: FontWeight.normal,
+                          color: Colors.grey.shade500,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                trailing: const FaIcon(
+                  FontAwesomeIcons.chevronRight,
+                  size: Sizes.size16,
+                  color: Colors.black,
+                ),
               ),
             ),
-          ),
         ],
       ),
     );
