bce464a72c2f8ce9c44a158baa210d436457b640
Heungseop <imt300@naver.com>
2023-06-12 16:22:43 +0900

    13.2 showDateRangePicker

--- lib/features/settings/settingsScreen.dart
+++ lib/features/settings/settingsScreen.dart
@@ -10,20 +10,43 @@ class SettingsScreen extends StatelessWidget {
           title: const Text("Settings"),
         ),
         body: ListView(
-          children: const [
-            // ListTile(
-            //   onTap: () => showAboutDialog(
-            //       context: context,
-            //       applicationVersion: "1.0",
-            //       applicationLegalese:
-            //           "All rights reserved. Please don't copy me."),
-            //   title: const Text(
-            //     "About",
-            //     style: TextStyle(fontWeight: FontWeight.w600),
-            //   ),
-            //   subtitle: const Text("About this app..."),
-            // ),
-            AboutListTile()
+          children: [
+            ListTile(
+              onTap: () async {
+                final date = await showDatePicker(
+                  context: context,
+                  initialDate: DateTime.now(),
+                  firstDate: DateTime(1900),
+                  lastDate: DateTime(2030),
+                );
+                print("date : $date");
+
+                final time = await showTimePicker(
+                  context: context,
+                  initialTime: TimeOfDay.now(),
+                );
+                print("time : $time");
+
+                final booking = await showDateRangePicker(
+                  context: context,
+                  firstDate: DateTime(1900),
+                  lastDate: DateTime(2030),
+                  builder: (context, child) {
+                    return Theme(
+                        data: ThemeData(
+                          appBarTheme: const AppBarTheme(
+                            foregroundColor: Colors.white,
+                            backgroundColor: Colors.black,
+                          ),
+                        ),
+                        child: child!);
+                  },
+                );
+                print("booking : $booking");
+              },
+              title: const Text("What is your birthday?"),
+            ),
+            const AboutListTile()
           ],
         ));
   }
