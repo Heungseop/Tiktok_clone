d81ac78c3f26d99662042fddc3f4c5207a5997c8
Heungseop <imt300@naver.com>
2023-06-12 16:00:19 +0900

    13.0 ListWheelScrollView CircularProgressIndicator

--- lib/features/users/user_profile_screen.dart
+++ lib/features/users/user_profile_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:tiktok_clone/constants/gaps.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
+import 'package:tiktok_clone/features/settings/settingsScreen.dart';
 import 'package:tiktok_clone/features/users/widgets/persistent_tab_bar.dart';
 import 'package:tiktok_clone/features/users/widgets/user_info_button.dart';
 import 'package:tiktok_clone/features/users/widgets/user_info_card.dart';
@@ -15,6 +16,14 @@ class UserProfileScreen extends StatefulWidget {
 
 class _UserProfileScreenState extends State<UserProfileScreen> {
   final double gridMarkerGap = 5;
+
+  void _onGearPressed() {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (context) => const SettingsScreen(),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -33,7 +42,7 @@ class _UserProfileScreenState extends State<UserProfileScreen> {
                   centerTitle: true,
                   actions: [
                     IconButton(
-                      onPressed: () {},
+                      onPressed: _onGearPressed,
                       icon: const FaIcon(
                         FontAwesomeIcons.gear,
                         size: Sizes.size20,
