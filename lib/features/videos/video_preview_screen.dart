b27aac3d63979b0ea557b1941d6cd48351655f90
Heungseop <imt300@naver.com>
2023-06-16 15:43:22 +0900

    19.7 GallerySaver

--- lib/features/videos/video_preview_screen.dart
+++ lib/features/videos/video_preview_screen.dart
@@ -2,6 +2,8 @@ import 'dart:io';
 
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:gallery_saver/gallery_saver.dart';
 import 'package:video_player/video_player.dart';
 
 class VideoPreviewScreen extends StatefulWidget {
@@ -17,6 +19,8 @@ class VideoPreviewScreen extends StatefulWidget {
 
 class _VideoPreviewScreenState extends State<VideoPreviewScreen> {
   late final VideoPlayerController _videoPlayerController;
+  bool savedVideo = false;
+
   Future<void> _initVideo() async {
     _videoPlayerController = VideoPlayerController.file(
       File(widget.video.path),
@@ -42,11 +46,30 @@ class _VideoPreviewScreenState extends State<VideoPreviewScreen> {
     super.dispose();
   }
 
+  Future<void> _saveToGallery() async {
+    if (savedVideo) {
+      return;
+    }
+
+    GallerySaver.saveVideo(widget.video.path, albumName: "TikTok Clone!");
+
+    savedVideo = true;
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Preview video"),
+        actions: [
+          IconButton(
+            onPressed: _saveToGallery,
+            icon: FaIcon(
+              savedVideo ? FontAwesomeIcons.check : FontAwesomeIcons.download,
+            ),
+          )
+        ],
       ),
       body: _videoPlayerController.value.isInitialized
           ? VideoPlayer(_videoPlayerController)
