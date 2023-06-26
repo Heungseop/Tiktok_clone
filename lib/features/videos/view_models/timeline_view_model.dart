bf02a66f50b1d518efe284741b750ec7270b9077
Heungseop <imt300@naver.com>
2023-06-26 10:48:23 +0900

    22.4 AsyncNotifierProvider

---
+++ lib/features/videos/view_models/timeline_view_model.dart
@@ -0,0 +1,30 @@
+import 'dart:async';
+
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:tiktok_clone/features/videos/models/video_model.dart';
+
+class TimelineViewModel extends AsyncNotifier<List<VideoModel>> {
+  List<VideoModel> _list = [];
+
+  void uploadVideo() async {
+    state = const AsyncValue
+        .loading(); // => TimelineViewModel 이 다시 loading state가 되도록 만듬.
+    await Future.delayed(const Duration(seconds: 3));
+    final newVideo = VideoModel(title: "${DateTime.now()}");
+    _list = [..._list, newVideo]; //_list에 바로 add 할 수 없음(화면에 나타나지 않음)
+    state = AsyncValue.data(_list);
+  }
+
+  @override
+  FutureOr<List<VideoModel>> build() async {
+    await Future.delayed(const Duration(seconds: 3));
+    // throw Exception("omg cat fetch");
+    return _list;
+  }
+}
+
+//provider
+final timelineProvider =
+    AsyncNotifierProvider<TimelineViewModel, List<VideoModel>>(
+  () => TimelineViewModel(),
+);
--- lib/features/videos/views/video_timeline_screen.dart
+++ lib/features/videos/views/video_timeline_screen.dart
@@ -1,14 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:tiktok_clone/features/videos/view_models/timeline_view_model.dart';
 import 'package:tiktok_clone/features/videos/widgets/viedo_post.dart';
 
-class VideoTimeLineScreen extends StatefulWidget {
+class VideoTimeLineScreen extends ConsumerStatefulWidget {
   const VideoTimeLineScreen({super.key});
 
   @override
-  State<VideoTimeLineScreen> createState() => _VideoTimeLineScreenState();
+  VideoTimeLineScreenState createState() => VideoTimeLineScreenState();
 }
 
-class _VideoTimeLineScreenState extends State<VideoTimeLineScreen> {
+class VideoTimeLineScreenState extends ConsumerState<VideoTimeLineScreen> {
   int _itemCount = 4;
 
   final PageController _pageController = PageController();
@@ -48,20 +50,31 @@ class _VideoTimeLineScreenState extends State<VideoTimeLineScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return RefreshIndicator(
-      onRefresh: _onRefresh,
-      displacement: 50,
-      // edgeOffset: 21,
-      color: Theme.of(context).primaryColor,
-      child: PageView.builder(
-          controller: _pageController,
-          scrollDirection: Axis.vertical,
-          itemCount: _itemCount,
-          onPageChanged: _onPageChaged,
-          itemBuilder: (context, index) => VideoPost(
-                onVideoFinished: _onVideoFinished,
-                index: index,
-              )),
-    );
+    return ref.watch(timelineProvider).when(
+          loading: () => const Center(
+            child: CircularProgressIndicator(),
+          ),
+          error: (error, stackTrace) => Center(
+            child: Text(
+              "Could not load videos : $error",
+              // style: const TextStyle(color: Colors.white),
+            ),
+          ),
+          data: (videos) => RefreshIndicator(
+            onRefresh: _onRefresh,
+            displacement: 50,
+            // edgeOffset: 21,
+            color: Theme.of(context).primaryColor,
+            child: PageView.builder(
+                controller: _pageController,
+                scrollDirection: Axis.vertical,
+                itemCount: videos.length,
+                onPageChanged: _onPageChaged,
+                itemBuilder: (context, index) => VideoPost(
+                      onVideoFinished: _onVideoFinished,
+                      index: index,
+                    )),
+          ),
+        );
   }
 }
