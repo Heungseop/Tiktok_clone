cc54bb061a067efe24f18a0710bd201724a339cf
Heungseop <imt300@naver.com>
2023-06-30 11:52:22 +0900

    26.2 uploadVideoProvider

--- lib/features/videos/models/video_model.dart
+++ lib/features/videos/models/video_model.dart
@@ -4,6 +4,7 @@ class VideoModel {
   final String fileUrl;
   final String thumnailUrl;
   final String createUid;
+  final String creator;
   final int likes;
   final int comments;
   final int createdAt;
@@ -17,5 +18,20 @@ class VideoModel {
     required this.likes,
     required this.comments,
     required this.createdAt,
+    required this.creator,
   });
+
+  Map<String, dynamic> toJson() {
+    return {
+      "title": title,
+      "description": description,
+      "fileUrl": fileUrl,
+      "thumnailUrl": thumnailUrl,
+      "createUid": createUid,
+      "likes": likes,
+      "comments": comments,
+      "createdAt": createdAt,
+      "creator": creator,
+    };
+  }
 }
--- lib/features/videos/view_models/timeline_view_model.dart
+++ lib/features/videos/view_models/timeline_view_model.dart
@@ -4,7 +4,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:tiktok_clone/features/videos/models/video_model.dart';
 
 class TimelineViewModel extends AsyncNotifier<List<VideoModel>> {
-  List<VideoModel> _list = [];
+  final List<VideoModel> _list = [];
 
   // 비디오 업로드 메서드.
   // 자동/강제로 로딩상태를 유발시킴
@@ -12,8 +12,8 @@ class TimelineViewModel extends AsyncNotifier<List<VideoModel>> {
     state = const AsyncValue
         .loading(); // => TimelineViewModel 이 다시 loading state가 되도록 만듬.
     await Future.delayed(const Duration(seconds: 3));
-    final newVideo = VideoModel(title: "${DateTime.now()}");
-    _list = [..._list, newVideo]; //_list에 바로 add 할 수 없음(화면에 나타나지 않음)
+    // final newVideo = VideoModel(title: "${DateTime.now()}");
+    // _list = [..._list, newVideo]; //_list에 바로 add 할 수 없음(화면에 나타나지 않음)
     state = AsyncValue.data(_list);
   }
 
