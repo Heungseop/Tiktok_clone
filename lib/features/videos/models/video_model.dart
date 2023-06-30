3556c0f20b98844d541db149c99af534c3b2c35c
Heungseop <imt300@naver.com>
2023-06-30 11:06:58 +0900

    26.1 VideosRepository

--- lib/features/videos/models/video_model.dart
+++ lib/features/videos/models/video_model.dart
@@ -1,4 +1,21 @@
 class VideoModel {
-  String title;
-  VideoModel({required this.title});
+  final String title;
+  final String description;
+  final String fileUrl;
+  final String thumnailUrl;
+  final String createUid;
+  final int likes;
+  final int comments;
+  final int createdAt;
+
+  VideoModel({
+    required this.title,
+    required this.description,
+    required this.fileUrl,
+    required this.thumnailUrl,
+    required this.createUid,
+    required this.likes,
+    required this.comments,
+    required this.createdAt,
+  });
 }
