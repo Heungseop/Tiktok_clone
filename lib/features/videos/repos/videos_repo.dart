6a5d9059d3905f6be5b73656f5aeb61905aee985
heungseop <imt300@naver.com>
2023-07-13 11:56:14 +0900

    27.4 likeVideo

--- lib/features/videos/repos/videos_repo.dart
+++ lib/features/videos/repos/videos_repo.dart
@@ -36,10 +36,21 @@ class VideosRepository {
   }
 
   Future<void> likeVideo(String videoId, String userId) async {
-    await _db.collection("likes").add({
-      "videoId": videoId,
-      "userId": userId,
-    });
+    // like 한유저인지 찾는 작업이 firebase에선 매우 비싼 작업이 될 수 있기때문에 다른 접근이 필요함
+    // await _db
+    //     .collection("likes")
+    //     .where("videoId", isEqualTo: videoId)
+    //     .where("userId", isEqualTo: userId);
+
+    // 특정아이디로 도큐먼트를 만들어 저장하고 찾는다 Id를 찾는건 빠른가봄
+    final query = _db.collection("likes").doc("${videoId}000$userId");
+    final like = await query.get();
+
+    if (!like.exists) {
+      await query.set({
+        "createdAt": DateTime.now().millisecondsSinceEpoch,
+      });
+    }
   }
 }
 
