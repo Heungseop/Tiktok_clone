bf388c54a2058c401d81e9de7820a06667683623
heungseop <imt300@naver.com>
2024-01-21 15:20:10 +0900

    avatar custom, create new chat target bottom sheet

--- lib/features/inbox/widgets/create_a_new_chat_screen.dart
+++ lib/features/inbox/widgets/create_a_new_chat_screen.dart
@@ -4,8 +4,10 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:tiktok_clone/constants/breakepoints.dart';
 import 'package:tiktok_clone/constants/gaps.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
+import 'package:tiktok_clone/features/authentication/widgets/main_button.dart';
 import 'package:tiktok_clone/features/users/models/user_profile_model.dart';
 import 'package:tiktok_clone/features/users/view_models/users_view_model.dart';
+import 'package:tiktok_clone/features/users/views/widgets/avatar.dart';
 import 'package:tiktok_clone/utils.dart';
 
 class CreateANewChatScreen extends ConsumerStatefulWidget {
@@ -21,7 +23,7 @@ class CreateANewChatScreenState extends ConsumerState<CreateANewChatScreen> {
   final TextEditingController _textEditingController = TextEditingController();
   late final Future<List<UserProfileModel>> _users =
       ref.watch(usersProvider.notifier).fetchAllUsers();
-  List<int> selected = List.empty(growable: true);
+  List<UserProfileModel> selected = List.empty(growable: true);
 
   // void _fetchUsers() {
   //   _users = ref.watch(usersProvider.notifier).fetchAllUsers();
@@ -50,13 +52,23 @@ class CreateANewChatScreenState extends ConsumerState<CreateANewChatScreen> {
     _textEditingController.clear();
   }
 
-  void _userTap(int index) {
-    print("_userTap index : $index");
+  void _userTap(UserProfileModel user) {
+    print("_userTap user : $user");
     print("_userTap selected : $selected");
-    if (!selected.contains(index)) {
-      selected.add(index);
+    if (selected.isEmpty) {
+      // showBottomSheet(
+
+      //   // isScrollControlled: true,
+      //   // backgroundColor: Colors.transparent, // transparent 를 줌으로써 스캐폴드가 배경이 됨
+      //   context: context,
+      //   builder: (context) => const selectedChatUsersScreen(),
+      // );
+    }
+
+    if (!selected.contains(user)) {
+      selected.add(user);
     } else {
-      selected.remove(index);
+      selected.remove(user);
     }
     setState(() {});
     print("2_userTap selected : $selected");
@@ -83,137 +95,191 @@ class CreateANewChatScreenState extends ConsumerState<CreateANewChatScreen> {
         ),
       ),
       child: Scaffold(
+        backgroundColor: isDark ? null : Colors.grey.shade50,
+        appBar: AppBar(
           backgroundColor: isDark ? null : Colors.grey.shade50,
-          appBar: AppBar(
-            backgroundColor: isDark ? null : Colors.grey.shade50,
-            centerTitle: true,
-            title: const Text("Create a new chat"),
-            automaticallyImplyLeading: false,
-            actions: [
-              IconButton(
-                  onPressed: _onClossePressed,
-                  icon: const FaIcon(
-                    FontAwesomeIcons.xmark,
-                  ))
-            ],
-          ),
-          body: Column(
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Container(
-                    constraints: const BoxConstraints(
-                      maxWidth: Breakpoints.sm,
-                    ), // 마냥 늘어나는 크기를 제한해줄 수 있다.
-                    width: MediaQuery.of(context).size.width - Sizes.size24,
-                    height: Sizes.size40, // 인풋 높이
-                    child: TextField(
-                      // onSubmitted: _onSearchSubmitted,
-                      // onChanged: _onSearchChanged,
-                      controller: _textEditingController,
-                      // cursorColor: Theme.of(context).primaryColor,
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(Sizes.size12),
-                          borderSide: BorderSide.none, // 라인제거
-                        ),
-                        filled: true, // 인풋 색상 채울지 여부
-                        fillColor: isDarkMode(context)
-                            ? Colors.grey.shade800
-                            : Colors.grey.shade200,
-                        prefixIcon: Row(
-                          mainAxisSize: MainAxisSize.min,
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            FaIcon(
-                              FontAwesomeIcons.magnifyingGlass,
-                              // color: Colors.grey.shade500,
-                              color: isDarkMode(context)
-                                  ? Colors.grey.shade500
-                                  : Colors.black,
+          centerTitle: true,
+          title: const Text("Create a new chat"),
+          automaticallyImplyLeading: false,
+          actions: [
+            IconButton(
+                onPressed: _onClossePressed,
+                icon: const FaIcon(
+                  FontAwesomeIcons.xmark,
+                ))
+          ],
+        ),
+        body: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  constraints: const BoxConstraints(
+                    maxWidth: Breakpoints.sm,
+                  ), // 마냥 늘어나는 크기를 제한해줄 수 있다.
+                  width: MediaQuery.of(context).size.width - Sizes.size24,
+                  height: Sizes.size40, // 인풋 높이
+                  child: TextField(
+                    // onSubmitted: _onSearchSubmitted,
+                    // onChanged: _onSearchChanged,
+                    controller: _textEditingController,
+                    // cursorColor: Theme.of(context).primaryColor,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(Sizes.size12),
+                        borderSide: BorderSide.none, // 라인제거
+                      ),
+                      filled: true, // 인풋 색상 채울지 여부
+                      fillColor: isDarkMode(context)
+                          ? Colors.grey.shade800
+                          : Colors.grey.shade200,
+                      prefixIcon: Row(
+                        mainAxisSize: MainAxisSize.min,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          FaIcon(
+                            FontAwesomeIcons.magnifyingGlass,
+                            // color: Colors.grey.shade500,
+                            color: isDarkMode(context)
+                                ? Colors.grey.shade500
+                                : Colors.black,
+                            size: Sizes.size20,
+                          ),
+                        ],
+                      ),
+                      suffixIcon: Row(
+                        mainAxisSize: MainAxisSize.min,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          GestureDetector(
+                            onTap: _onSearchFieldReset,
+                            child: FaIcon(
+                              FontAwesomeIcons.solidCircleXmark,
+                              color: Colors.grey.shade600,
                               size: Sizes.size20,
                             ),
-                          ],
+                          ),
+                        ],
+                      ),
+                      contentPadding: const EdgeInsets.symmetric(
+                        horizontal: Sizes.size12,
+                        vertical: Sizes.size10,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Expanded(
+              child: Stack(
+                children: [
+                  Scrollbar(
+                    controller: _scrollController,
+                    child: FutureBuilder(
+                      future: _users,
+                      builder: (context, snapshot) => ListView.separated(
+                        controller: _scrollController,
+                        padding: const EdgeInsets.only(
+                          top: Sizes.size10,
+                          bottom: Sizes.size10 + Sizes.size96,
+                          left: Sizes.size16,
+                          right: Sizes.size16,
                         ),
-                        suffixIcon: Row(
-                          mainAxisSize: MainAxisSize.min,
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            GestureDetector(
-                              onTap: _onSearchFieldReset,
-                              child: FaIcon(
-                                FontAwesomeIcons.solidCircleXmark,
-                                color: Colors.grey.shade600,
-                                size: Sizes.size20,
+                        separatorBuilder: (context, index) => Gaps.v1,
+                        itemCount:
+                            snapshot.data == null ? 0 : snapshot.data!.length,
+                        itemBuilder: (context, index) {
+                          UserProfileModel seletedUser = snapshot.data![index];
+                          return ListTile(
+                            onTap: () => _userTap(seletedUser),
+                            leading: CircleAvatar(
+                              radius: 18,
+                              backgroundColor:
+                                  isDark ? Colors.grey.shade800 : null,
+                              child: Text("H$index"),
+                            ),
+                            title: Text(
+                              seletedUser.name,
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: Sizes.size14,
+                                color: Colors.grey.shade500,
                               ),
                             ),
-                          ],
-                        ),
-                        contentPadding: const EdgeInsets.symmetric(
-                          horizontal: Sizes.size12,
-                          vertical: Sizes.size10,
-                        ),
+                            subtitle: Text(seletedUser.email),
+                            trailing: FaIcon(
+                              selected.contains(seletedUser)
+                                  ? FontAwesomeIcons.solidCircleCheck
+                                  : FontAwesomeIcons.circleCheck,
+                              size: Sizes.size20,
+                              color: selected.contains(seletedUser)
+                                  ? Theme.of(context).primaryColor
+                                  : Colors.grey.shade500,
+                            ),
+                          );
+                        },
                       ),
                     ),
                   ),
                 ],
               ),
-              Expanded(
-                child: Stack(
+            ),
+          ],
+        ),
+        bottomSheet: selected.isEmpty
+            ? null
+            : SizedBox(
+                height: Sizes.size80 + Sizes.size80 + Sizes.size60,
+                child: Column(
                   children: [
-                    Scrollbar(
-                      controller: _scrollController,
-                      child: FutureBuilder(
-                        future: _users,
-                        builder: (context, snapshot) => ListView.separated(
-                          controller: _scrollController,
-                          padding: const EdgeInsets.only(
-                            top: Sizes.size10,
-                            bottom: Sizes.size10 + Sizes.size96,
-                            left: Sizes.size16,
-                            right: Sizes.size16,
-                          ),
-                          separatorBuilder: (context, index) => Gaps.v1,
-                          itemCount:
-                              snapshot.data == null ? 0 : snapshot.data!.length,
-                          itemBuilder: (context, index) {
-                            return ListTile(
-                              onTap: () => _userTap(index),
-                              leading: CircleAvatar(
-                                radius: 18,
-                                backgroundColor:
-                                    isDark ? Colors.grey.shade800 : null,
-                                child: Text("H$index"),
-                              ),
-                              title: Text(
-                                snapshot.data![index].name,
-                                style: TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: Sizes.size14,
-                                  color: Colors.grey.shade500,
-                                ),
-                              ),
-                              subtitle: Text(snapshot.data![index].email),
-                              trailing: FaIcon(
-                                selected.contains(index)
-                                    ? FontAwesomeIcons.solidCircleCheck
-                                    : FontAwesomeIcons.circleCheck,
-                                size: Sizes.size20,
-                                color: selected.contains(index)
-                                    ? Theme.of(context).primaryColor
-                                    : Colors.grey.shade500,
-                              ),
-                            );
-                          },
+                    Container(
+                      padding: const EdgeInsets.all(Sizes.size18),
+                      // height: Sizes.size80,
+                      // clipBehavior: Clip.hardEdge,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(
+                          Sizes.size14,
                         ),
                       ),
+                      child: Row(
+                        children: [
+                          for (UserProfileModel user in selected)
+                            Container(
+                              padding: const EdgeInsets.symmetric(
+                                  horizontal: Sizes.size10),
+                              child: Avatar(
+                                uid: user.uid,
+                                name: user.name,
+                                hasAvatar: user.hasAvatar,
+                                isEditable: false,
+                                size: Sizes.size32,
+                              ),
+
+                              // CircleAvatar(
+                              //   radius: Sizes.size20,
+                              //   // foregroundImage:
+                              //   //     user.hasAvatar ? user.bio : null,
+                              //   backgroundColor:
+                              //       isDark ? Colors.grey.shade800 : null,
+                              //   child: Text(
+                              //     user.name,
+                              //     textScaleFactor: 0.7,
+                              //   ),
+                              // ),
+                            ),
+                        ],
+                      ),
                     ),
+                    const Padding(
+                        padding: EdgeInsets.all(Sizes.size18),
+                        child: MainButton(
+                          text: "Chat",
+                        )),
                   ],
                 ),
               ),
-            ],
-          )),
+      ),
     );
   }
 }
