3142c8b87ee439fb3bc8cea2d27be755a7b9b808
Heungseop <imt300@naver.com>
2024-08-02 16:11:22 +0900

    30.7 Integration Testing part Two

--- integration_test/integration_test.dart
+++ integration_test/integration_test.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
@@ -33,18 +34,39 @@ void main() {
 
     //다른화면 발생
     await tester.tap(find.text("Log in"));
-    await tester.pumpAndSettle(const Duration(seconds: 5));
+    await tester.pumpAndSettle();
 
     // login screen-------------------------------------------------------------------------------------------
     final singUp = find.text("Sign Up");
     expect(singUp, findsOneWidget);
     await tester.tap(singUp);
-    await tester.pumpAndSettle(const Duration(seconds: 5));
+    await tester.pumpAndSettle();
 
     final emailBtn = find.text("Use Email & Password");
     expect(emailBtn, findsOneWidget);
     await tester.tap(emailBtn);
-    await tester.pumpAndSettle(const Duration(seconds: 5));
+    await tester.pumpAndSettle();
+
+    final usernameInput = find.byType(TextField).first;
+    await tester.enterText(usernameInput, "test");
+    await tester.pumpAndSettle();
+    await tester.tap(find.text("Next"));
+    await tester.pumpAndSettle();
+
+    final emailInput = find.byType(TextField).first;
+    await tester.enterText(emailInput, "test@testing.com");
+    await tester.pumpAndSettle();
+    await tester.tap(find.text("Next"));
+    await tester.pumpAndSettle();
+
+    final passwordInput = find.byType(TextField).first;
+    await tester.enterText(passwordInput, "1q2w3e4r5t!");
+    await tester.pumpAndSettle();
+    await tester.tap(find.text("Next"));
+    await tester.pumpAndSettle();
+    await tester.tap(find.text("Next"));
+    await tester.pumpAndSettle();
+    await tester.pumpAndSettle(const Duration(seconds: 10));
   });
 
   //2단계
